eb71f1becd19f754b8085bcffbd6bfce696359d3
peppelinux <giuseppe.demarco@unical.it>
2022-03-27 23:21:34 +0200

    fix: dependencies cleanup

--- uniticket/accounts/models.py
+++ uniticket/accounts/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django_countries.fields import CountryField
 
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext as _
-from django.contrib.contenttypes.models import ContentType
 
 from django.conf import settings
 
@@ -33,22 +31,7 @@ class User(AbstractUser):
     taxpayer_id = models.CharField(_('TaxPayer ID - codice fiscale'),
                                       max_length=50,
                                       blank=True, null=True)
-    # gender    = models.CharField(_('Genere'), choices=GENDER,
-                                 # max_length=12, blank=True, null=True)
-    # place_of_birth = CountryField('Luogo di nascita', max_length=30,
-                            # blank=True, null=True)
-    # birth_date = models.DateField('Data di nascita',
-                                  # null=True, blank=True)
     email_notify = models.BooleanField(_('Notifiche mail'), default=True)
-
-    # fields to map roles
-    # is_operator = models.BooleanField(_('Operatore interno'), default=False)
-    # is_utente = models.BooleanField(_('Utente organizzazione'), default=False)
-
-    # short_description = models.CharField(_('Descrizione breve'), max_length=33, blank=True, null=True)
-    # bio = models.TextField('Biografia, note', max_length=2048, blank=True, null=True)
-    # avatar  = models.ImageField('Avatar, foto', upload_to='avatars/', null=True, blank=True)
-    # webpage_url = models.CharField(_('Pagina web'), max_length=512, blank=True, null=True)
 
     class Meta:
         ordering = ['last_name', 'first_name']
