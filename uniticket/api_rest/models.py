c5e74f0c0fd7dfb994c7c02efe71177db4bab6ab
peppelinux <giuseppe.demarco@unical.it>
2022-04-13 23:12:15 +0200

    feat: Token authz in API

--- uniticket/api_rest/models.py
+++ uniticket/api_rest/models.py
@@ -1,8 +1,17 @@
+import string
+import random
+
 from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
+
+
+def create_token(length:int = 128) -> str:
+    return ''.join(
+        random.sample(string.hexdigits*12, length)
+    )
 
 
 class AuthorizationToken(models.Model):
@@ -25,8 +34,15 @@ class AuthorizationToken(models.Model):
         ),
         on_delete=models.CASCADE
     )
+    value = models.CharField(
+        max_length=128, blank=False, unique=True,
+        default=create_token,
+        help_text=_(
+            "Value of the Token"
+        )
+    )
     active_until = models.DateTimeField()
-    note = models.TextField(null=True,blank=True)
+    note = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
@@ -43,4 +59,4 @@ class AuthorizationToken(models.Model):
             return False
 
     def __str__(self):
-        return "{self.name} [{self.user}]"
+        return f"{self.name} [{self.user}]"
